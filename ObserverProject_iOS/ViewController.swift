01aa9c8c4ae1aaef2cd3e2950358443f09324cc4
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-09 14:50:14 +0600

    Initial Commit

---
+++ ObserverProject_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObserverProject_iOS
+//
+//  Created by Sadia on 9/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
